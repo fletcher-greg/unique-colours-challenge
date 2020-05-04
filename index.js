f2f3d40502aadcb9885f707cea26fc31ad6d3865
fletcher-greg <fletcher.web.dev.one@gmail.com>
2020-05-04 04:51:03 +0200

    clean up comments

--- index.js
+++ index.js
@@ -5,18 +5,16 @@ const ctx = canvas.getContext("2d");
 const worker = new Worker("./worker.js");
 
 // createColours calls a web worker and gives it image data to calculate.
-// web workers do not run on the main thread. This means the calculation will not cause the UI to due to large javascript calculations
 // when the web worker returns the data the canvas is updated
 
 createColours();
-// event listener
 
 function createColours() {
   // get canvas data
   let imageData = ctx.getImageData(0, 0, 256, 128);
   // pass data to web worker
   worker.postMessage({ imageData });
-  // web worker has returned data
+  // web worker returns data
   worker.addEventListener("message", (e) => {
     // update canvas with data
     ctx.putImageData(e.data.imageData, 0, 0);
